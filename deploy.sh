ceb7788b4fe7a78f95fdbc48d0105f7546b79346
Max Lielje <max@maxlielje.co>
2015-04-02 02:39:51 +0200

    rotate keys

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-echo "Deploying to viaduct"
-curl  -H "X-Auth-Token:  $VDT_AUTH_TOKEN"         \
-      -H "X-Auth-Secret: $VDT_AUTH_SECRET"        \
-      -X POST                                     \
-      -d 'params={"application":"dd2569c0bcdd"}'  \
-      http://eu.httpbin.org/post
+echo "Deploying to Viaduct, hang tight."
+curl  -H "X-Auth-Token:  $VDT_AUTH_TOKEN" \
+      -H "X-Auth-Secret: $VDT_AUTH_SECRET" \
+      -X POST \
+      -d 'params={"application":"dd2569c0bcdd"}' \
+      https://api.viaduct.io/api/v1/deployments/start
