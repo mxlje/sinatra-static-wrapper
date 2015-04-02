04bfd8ffb29a8e0265555062f9c14d8d5948890a
Max Lielje <max@maxlielje.co>
2015-04-02 02:54:39 +0200

    deploy script changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-echo "Deploying to Viaduct, hang tight."
-curl  -H "X-Auth-Token:  $VDT_AUTH_TOKEN" \
-      -H "X-Auth-Secret: $VDT_AUTH_SECRET" \
-      -X POST \
-      -d 'params={"application":"dd2569c0bcdd"}' \
-      https://api.viaduct.io/api/v1/deployments/start
+echo "Deploying to Viaduct"
+
+curl -H "X-Auth-Token: $VDT_AUTH_TOKEN" -H "X-Auth-Secret: $VDT_AUTH_SECRET" -X POST -d 'params={"application":"dd2569c0bcdd"}' https://api.viaduct.io/api/v1/deployments/start
